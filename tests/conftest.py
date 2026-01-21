8efd382fca62d0a096cc81950ba4054f3a41192b
podpitkin <nodmva@gmail.com>
2026-01-21 23:51:18 +0300

    HW_14_1

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,18 @@ import pytest
 from src.category import Category
 from src.product import Product
 
+
 @pytest.fixture
 def prod():
-    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    return Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+
 
 @pytest.fixture
-
 def first_cat():
-    return Category("Смартфоны",
-                    "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                    []
-                    )
+    return Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [],
+    )
--- tests/test_category.py
+++ tests/test_category.py
@@ -1,8 +1,12 @@
 from src.category import Category
 
+
 def test_category_init(first_cat):
     assert first_cat.name == "Смартфоны"
-    assert first_cat.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    assert (
+        first_cat.description
+        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    )
     assert first_cat.product == []
     assert Category.category_count == 1
     assert Category.product_count == 0
