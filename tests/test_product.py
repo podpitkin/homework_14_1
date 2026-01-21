7e26cfe42f8a9c4d1b90d8b3cb22f9bdb1c8d6f5
podpitkin <nodmva@gmail.com>
2026-01-21 21:48:32 +0300

    HW_14_1

---
+++ tests/test_product.py
@@ -0,0 +1,6 @@
+
+def test_product(prod):
+    assert prod.name == "Samsung Galaxy S23 Ultra"
+    assert prod.description == "256GB, Серый цвет, 200MP камера"
+    assert prod.price == 180000.0
+    assert prod.quantity == 5
