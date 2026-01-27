9523a0232d703e25fa4601ce8a4b47e04dcc47fd
podpitkin <nodmva@gmail.com>
2026-01-27 22:59:26 +0300

    HW_14_2

--- src/category.py
+++ src/category.py
@@ -1,3 +1,4 @@
+
 class Category:
     name: str
     description: str
@@ -8,6 +9,17 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.product = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        product_list = ''
+        for product in self.__products:
+            product_list = f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.'
+        return product_list
